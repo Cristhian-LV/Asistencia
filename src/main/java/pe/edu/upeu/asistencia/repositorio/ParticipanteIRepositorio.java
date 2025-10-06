9ef7f71b7fff33fd3f55408376127039439b7750
Cristhian-LV <thiancris4@gmail.com>
2025-10-05 21:31:48 -0500

    jpa

--- src/main/java/pe/edu/upeu/asistencia/repositorio/ParticipanteIRepositorio.java
+++ src/main/java/pe/edu/upeu/asistencia/repositorio/ParticipanteIRepositorio.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.upeu.asistencia.modelo.Participante;
 
-//@Repository
+@Repository
 public interface ParticipanteIRepositorio extends JpaRepository<Participante,String> {
 }
